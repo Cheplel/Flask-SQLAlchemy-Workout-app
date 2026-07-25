9c401790b570764eb541447aff45375424759161
Jean <jean.koech@student.moringaschool.com>
2026-07-25 23:15:51 +0300

    WorkoutExercises model created

--- server/models.py
+++ server/models.py
@@ -23,3 +23,15 @@ class Workout(db.Model):
     exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
     exercise = db.relationship('Exercise', back_populates='workouts')
 
+class WorkoutExercises(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
+    workout = db.relationship('Workout', backref=db.backref('workout_exercises', cascade='all, delete-orphan'))
+    exercise = db.relationship('Exercise', backref=db.backref('workout_exercises', cascade='all, delete-orphan'))
