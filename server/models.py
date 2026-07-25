a5540005b41afdb64151302e8e17d24fb370071b
Jean <jean.koech@student.moringaschool.com>
2026-07-25 23:13:59 +0300

    Workout model created

--- server/models.py
+++ server/models.py
@@ -8,6 +8,18 @@ class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     category = db.Column(db.String)
-    equipment = db.Column(db.String)
+    equipment = db.Column(db.Boolean)
 
     workouts = db.relationship('Workout', back_populates='exercise')
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    duration = db.Column(db.Integer)  # duration in minutes
+    notes = db.Column(db.text)
+
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
+    exercise = db.relationship('Exercise', back_populates='workouts')
+
