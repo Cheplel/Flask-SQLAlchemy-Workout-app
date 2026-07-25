5cb54ca9660b143d2995f0cd57147951660073a2
Jean <jean.koech@student.moringaschool.com>
2026-07-25 23:09:40 +0300

    Exercise model created

--- server/models.py
+++ server/models.py
@@ -1,3 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
+
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment = db.Column(db.String)
+
+    workouts = db.relationship('Workout', back_populates='exercise')
